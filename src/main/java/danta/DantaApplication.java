4559f9ff506ecc17b5ec06b60d67e51206bb1a50
문지원(JiwonMoon) <mon99745@gmail.com>
2022-04-11 15:21:48 +0900

    Setting the development environment

---
+++ src/main/java/danta/DantaApplication.java
@@ -0,0 +1,13 @@
+package danta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danta/DantaApplicationTests.java
@@ -0,0 +1,13 @@
+package danta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
