bc8e99941a0aee05e8b3c49f065237f0d5f4c11f
jiwon Moon <mon99745@dreamsecurity.com>
2023-04-26 10:52:37 +0900

    update. Id 타입 수정 (String -> Long)

--- src/main/java/danta/repository/UserRepository.java
+++ src/main/java/danta/repository/UserRepository.java
@@ -4,5 +4,5 @@ import danta.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository
-        extends JpaRepository<User, String> {
+        extends JpaRepository<User, Long> {
 }
