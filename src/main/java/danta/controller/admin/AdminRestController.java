9dcf73d2f7b02db33611378b5d98830bdc2551f5
jiwon Moon <mon99745@dreamsecurity.com>
2023-04-26 16:52:13 +0900

    add. Admin API 추가

---
+++ src/main/java/danta/controller/admin/AdminRestController.java
@@ -0,0 +1,81 @@
+package danta.controller.admin;
+
+import danta.model.Admin;
+import danta.model.User;
+import danta.service.admin.AdminService;
+import danta.service.user.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Swagger 사용을 위해 모든 권한(authh) 을 허용한 상태
+ */
+
+@Slf4j
+@Api(tags = AdminRestController.TAG, description = "회원 로직을 관리하는 Rest API")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RestController
+@RequestMapping(AdminRestController.PATH)
+public class AdminRestController {
+
+    @Value("${security.auth}")
+    private boolean auth;
+    public static final String PATH = "/auth/api/v1/admin";
+    public static final String TAG = "Admin Rest API";
+    private final AdminService adminServie;
+    private final UserService userService;
+
+
+    /**
+     * 관리자 가입 API
+     */
+    @Operation(summary = "1. 관리자 가입")
+    @PostMapping("create")
+    public Long create(@RequestBody Admin admin) {
+        return adminServie.save(admin);
+    }
+
+    /**
+     * 관리자 조회 API
+     */
+    @Operation(summary = "2. 관리자 조회")
+    @GetMapping("read/{adminId}")
+    public Admin read(@PathVariable Long adminId) {
+        return adminServie.findAdmin(adminId);
+    }
+
+    /**
+     * 관리자 수정 API
+     */
+    @Operation(summary = "3. 관리자 정보 수정")
+    @PutMapping("update/{adminId}")
+    public Admin update(@PathVariable Long adminId, @RequestBody Admin admin) {
+        return adminServie.update(adminId, admin);
+    }
+
+    /**
+     * 관리자 탈퇴 API
+     */
+    @Operation(summary = "4. 관리자 탈퇴 ")
+    @DeleteMapping("delete/{adminId}")
+    public void delete(@PathVariable Long adminId, @RequestBody Admin admin) {
+        adminServie.delete(adminId, admin);
+    }
+
+    /**
+     * 전체 회원 조회 API (관리자용)
+     */
+    @Operation(summary = "5. 전체 회원 조회 (관리자용)")
+    @GetMapping("readList")
+    public List<User> list(){
+        List<User> userList = userService.findAll();
+        return userList;
+    }
+}
--- src/main/java/danta/repository/AdminRepository.java
+++ src/main/java/danta/repository/AdminRepository.java
@@ -1,7 +1,9 @@
 package danta.repository;
 
 import danta.model.Admin;
+import danta.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByAdminname(String username);
 }
