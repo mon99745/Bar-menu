5b6647216346aff710400056f65ac6b51d211828
jiwon Moon <mon99745@dreamsecurity.com>
2023-04-26 11:35:46 +0900

    update. User 엔티티 수정

--- src/main/java/danta/model/User.java
+++ src/main/java/danta/model/User.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.persistence.*;
 
@@ -24,27 +25,36 @@ import javax.persistence.*;
 @DynamicUpdate
 @Table(name = "UserInfo", indexes = {
         @Index(name = "idx_user_id", columnList = "user_id", unique = true),
-        @Index(name = "idx_user_password", columnList = "password"),
-        @Index(name = "idx_user_name", columnList = "name"),
-        @Index(name = "idx_user_status", columnList = "status"),
-        @Index(name = "idx_user_reg_date", columnList = "regDate"),
-        @Index(name = "idx_user_mod_date", columnList = "modDate")})
+        @Index(name = "idx_username", columnList = "username"),
+        @Index(name = "idx_password", columnList = "password"),
+        @Index(name = "idx_name", columnList = "name"),
+        @Index(name = "idx_status", columnList = "status"),
+        @Index(name = "idx_reg_date", columnList = "regDate"),
+        @Index(name = "idx_mod_date", columnList = "modDate")})
 public class User extends AbstractModel {
 
     /**
-     * 회원 ID
+     * 회원 식별 번호
      */
     @Id
-    @Schema(description = "회원 ID")
+    @Column(name = "user_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.TABLE)
-    @Column(name = "user_id",nullable = false)
     protected Long id;
 
     /**
-     * 회원 비밀번호
-     * TODO 암호화 함수 추가
+     * 회원 ID
+     * TODO 암호화
      */
     @JsonProperty(index = 10)
+    @Schema(description = "회원 ID")
+    @Column(length = 100, nullable = false)
+    protected String username;
+
+    /**
+     * 회원 비밀번호
+     * TODO 암호화
+     */
+    @JsonProperty(index = 20)
     @Schema(description = "회원 비밀번호")
     @Column(length = 100)
     protected String password;
@@ -52,15 +62,16 @@ public class User extends AbstractModel {
     /**
      * 회원 이름
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 30)
     @Schema(description = "회원 이름")
     @Column(length = 100)
     protected String name;
 
     /**
      * 회원 상태
+     * TODO ENUM
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 40)
     @Schema(description = "회원 상태")
     @Column(length = 100)
     protected String status;
