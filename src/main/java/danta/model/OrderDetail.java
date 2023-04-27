e6ec45e8569961a3fbb901fdaa07bc770a753357
jiwon Moon <mon99745@dreamsecurity.com>
2023-04-27 11:27:42 +0900

    fix. 중복 매핑 컬럼 삭제

--- src/main/java/danta/model/OrderDetail.java
+++ src/main/java/danta/model/OrderDetail.java
@@ -21,32 +21,29 @@ import javax.persistence.*;
 @SuperBuilder
 @NoArgsConstructor
 @Entity
-@Embeddable
 @DynamicInsert
 @DynamicUpdate
 @Component
 @Table(name = "OrderDetail", indexes = {
-        @Index(name = "idx_orderDetail_id", columnList = "orderDeatil_id", unique = true),
+        @Index(name = "idx_orderDetail_id", columnList = "orderDetail_id", unique = true),
         @Index(name = "idx_orderDetail_order_id", columnList = "order_id"),
         @Index(name = "idx_orderDetail_product_id", columnList = "product_id"),
         @Index(name = "idx_orderDetail_orderQuantity", columnList = "orderQuantity"),
-        @Index(name = "idx_orderDetail_orderPrice", columnList = "orderPrice"),
-        @Index(name = "idx_orderDetail_reg_date", columnList = "regDate"),
-        @Index(name = "idx_orderDetail_mod_date", columnList = "modDate")})
-public class OrderDetail extends AbstractModel {
+        @Index(name = "idx_orderDetail_orderPrice", columnList = "orderPrice")})
+public class OrderDetail{
 
     /**
      * 주문 상세 ID
      */
     @Id
     @Schema(description = "주문 상세 ID")
-    @Column(name = "orderDeatil_id", nullable = false)
+    @Column(name = "orderDetail_id", nullable = false)
     protected Long id;
 
     /**
      * 주문 ID
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 20)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     protected Order orderId;
@@ -54,7 +51,7 @@ public class OrderDetail extends AbstractModel {
     /**
      * 상품 ID
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 30)
     @Schema(description = "상품 ID")
     @JoinColumn(name = "product_id")
     protected Product productId;
@@ -62,7 +59,7 @@ public class OrderDetail extends AbstractModel {
     /**
      * 주문 수량
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 40)
     @Schema(description = "주문 수량")
     @Column(length = 100)
     protected int orderQuantity;
@@ -70,7 +67,7 @@ public class OrderDetail extends AbstractModel {
     /**
      * 주문 가격
      */
-    @JsonProperty(index = 10)
+    @JsonProperty(index = 50)
     @Schema(description = "주문 가격")
     @Column(length = 100)
     protected int orderPrice;
