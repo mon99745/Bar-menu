c3336df0cdd02e0967530aae49d3066db1f4cf40
jiwon Moon <mon99745@dreamsecurity.com>
2023-04-28 17:45:49 +0900

    update. 파일 업로드 컨트롤러 추가

---
+++ src/main/java/danta/controller/file/FileUploadController.java
@@ -0,0 +1,35 @@
+package danta.controller.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+    @Value("file.uploadDir")
+    String uploadDir;
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+
+        if (file.isEmpty()) {
+            return "Please select a file to upload.";
+        }
+
+        try {
+            // 파일 저장 경로 지정
+            File dest = new File(uploadDir + file.getOriginalFilename());
+            // 파일 저장
+            file.transferTo(dest);
+            return "File uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to upload file.";
+        }
+    }
+}
